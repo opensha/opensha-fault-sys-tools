07500637031dcd1eb2b3f2660cd95121c13c8d70
Kevin Milner <kmilner@usc.edu>
2021-10-14 16:02:29 -0700

    added script for average solutions

---
+++ sbin/fst_solution_averager.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+$DIR/fst_jar_wrapper.sh org.opensha.sha.earthquake.faultSysSolution.util.AverageSolutionCreator $@
